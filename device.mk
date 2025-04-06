ab0a62165fdea2580360a5fe258b3ae72c9ac55c
wodanesdag <wodanesdag@proton.me>
2025-04-06 22:47:49 +0700

    ruby: Disable Matlog

--- device.mk
+++ device.mk
@@ -148,6 +148,9 @@ PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(DEVICE_PATH)/configs/seccomp,$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy) \
     $(call find-copy-subdir-files,*,$(DEVICE_PATH)/configs/media,$(TARGET_COPY_OUT_VENDOR)/etc)
 
+# Matlog
+TARGET_DISABLE_MATLOG := true
+
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc-service.st \
